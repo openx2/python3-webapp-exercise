29f6a4dc9a31763c29c427c945736719ded4a1a4
openx2 <xiaoyanghaitao@outlook.com>
2016-07-05 15:45:22 +0800

    Day 8:构建前端

--- www/handlers.py
+++ www/handlers.py
@@ -3,13 +3,20 @@
 
 __author__ = 'cx'
 
+import time
+
 from coroweb import get, post
-from models import User
+from models import User, Blog, Comment
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, create_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, create_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, create_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
